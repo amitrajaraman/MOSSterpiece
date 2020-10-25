be52d32820dce997cca3bd0f54c19c57acdb6601
AkashCherukuri <190050009@iitb.ac.in>
2020-10-26 05:01:11 +0530

    Registration works!

--- DjangoAPI/DjangoAPI/settings.py
+++ DjangoAPI/DjangoAPI/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_405(i+dp-ivj^i=4l4tr4bsn7+b#x!y7kc$o-jb^(^=f_%bf!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
