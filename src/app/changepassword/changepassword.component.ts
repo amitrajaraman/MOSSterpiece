2a96b153613dc3e249a9a160fc5ef73c3b044dc4
vigna-s <63233201+vigna-s@users.noreply.github.com>
2020-12-03 14:41:12 +0530

    change password-django part done

--- src/app/changepassword/changepassword.component.ts
+++ src/app/changepassword/changepassword.component.ts
@@ -1,63 +1,15 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { MessengerService, SharedService } from '../shared.service';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { Subscription } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-changepassword',
   templateUrl: './changepassword.component.html',
   styleUrls: ['./changepassword.component.scss']
 })
+export class ChangepasswordComponent implements OnInit {
 
-export class LoginComponent implements OnInit, OnDestroy {
-
-  private messageSubscription: Subscription;
-  public messages: string;
-  form_username = new FormControl('');
-  form_password = new FormControl('');
-
-  constructor(private service:SharedService, private router: Router, private messengerService: MessengerService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.messageSubscription = this.messengerService.message.subscribe(m => { this.messages = m });
-  }
-
-  ngOnDestroy() {
-    this.messageSubscription.unsubscribe();
-  }
-
-  setGlobalValue(value: string) {
-    this.messengerService.setMessage(value);
-  }
-
-  reroute_onLogin(value: string):void{
-    this.setGlobalValue(value);
-    this.router.navigate(['/profile']);
-  }
-
-  login():void{
-      var data = {
-          username: this.form_username.value,
-          password: this.form_password.value
-        }
-      if(this.messages==''){
-        this.service.login(data).subscribe(
-          (res)=>{
-          console.log(res);
-          window.alert("Logged in Successfully!");
-          this.reroute_onLogin(res.token);
-        },
-        (error) => {
-          window.alert("Incorrect Username/Password.");
-          console.log(error);
-          }
-        );
-      }
-      else{
-        window.alert("You are already logged in, please signout first.");
-      }
-      
   }
 
 }
