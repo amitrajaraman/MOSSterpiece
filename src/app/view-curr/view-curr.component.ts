0c564064569d7cc8b2e76576e70de111a84365df
Akash Cherukuri <62202731+AkashCherukuri@users.noreply.github.com>
2020-12-05 19:13:29 +0530

    small update to gitignore

--- src/app/view-curr/view-curr.component.ts
+++ src/app/view-curr/view-curr.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MessengerService } from '../shared.service';
 import { ResultService } from '../shared.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-view-curr',
@@ -9,12 +10,16 @@ import { ResultService } from '../shared.service';
 })
 export class ViewCurrComponent implements OnInit {
   result:Blob;
-  constructor(public messengerService: MessengerService, public resultservice: ResultService) { }
+  constructor(public messengerService: MessengerService, public resultservice: ResultService, private http:HttpClient) { }
 
   ngOnInit(): void {
     //Get the results from the data
     this.result = this.resultservice.getMessage();
     console.log(this.result);
+
+    //Read the results csv file
+    this.http.get('../../assets/results/outpFile.csv', {responseType: 'text'})
+        .subscribe(data => console.log(data));
   }
 
 }
