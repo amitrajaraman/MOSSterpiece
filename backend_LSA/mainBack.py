d6480ade064c51de5bbabbfcfcef9a63779e34d4
amitrajaraman <amit.rajaraman@gmail.com>
2020-12-06 17:58:58 +0530

    made sliiightly better

--- backend_LSA/mainBack.py
+++ backend_LSA/mainBack.py
@@ -6,41 +6,47 @@ import numpy as np
 import scipy.spatial.distance
 from zipfile import ZipFile
 import matplotlib.pyplot as plt
+# import ast
 
 def comment_remover_py(fname):
-    """ Run on just one file.
-    """
-    source = open(fname)
-    mod = open(fname + ",strip", "w")
+	source = open(fname)
+	dest = 'temp'
+	mod = open(dest, 'w')
 
-    prev_toktype = token.INDENT
-    first_line = None
-    last_lineno = -1
-    last_col = 0
+	prev_toktype = token.INDENT
+	first_line = None
+	last_lineno = -1
+	last_col = 0
 
-    tokgen = tokenize.generate_tokens(source.readline)
-    for toktype, ttext, (slineno, scol), (elineno, ecol), ltext in tokgen:
-        if 0:   # Change to if 1 to see the tokens fly by.
-            print("%10s %-14s %-20r %r" % (
-                tokenize.tok_name.get(toktype, toktype),
-                "%d.%d-%d.%d" % (slineno, scol, elineno, ecol),
-                ttext, ltext
-                ))
-        if slineno > last_lineno:
-            last_col = 0
-        if scol > last_col:
-            mod.write(" " * (scol - last_col))
-        if toktype == token.STRING and prev_toktype == token.INDENT:
-            # Docstring
-            mod.write("#--")
-        elif toktype == tokenize.COMMENT:
-            # Comment
-            mod.write("##\n")
-        else:
-            mod.write(ttext)
-        prev_toktype = toktype
-        last_col = ecol
-        last_lineno = elineno
+	tokgen = tokenize.generate_tokens(source.readline)
+	for toktype, ttext, (slineno, scol), (elineno, ecol), ltext in tokgen:
+		if 0:   # Change to if 1 to see the tokens fly by.
+			print("%10s %-14s %-20r %r" % (
+				tokenize.tok_name.get(toktype, toktype),
+				"%d.%d-%d.%d" % (slineno, scol, elineno, ecol),
+				ttext, ltext
+			))
+		if slineno > last_lineno:
+			last_col = 0
+		if scol > last_col:
+			mod.write(" " * (scol - last_col))
+		if toktype == token.STRING and prev_toktype == token.INDENT:
+			# Docstring
+			mod.write("\n")
+		elif toktype == tokenize.COMMENT:
+			# Comment
+			mod.write("\n")
+		else:
+			mod.write(ttext)
+		prev_toktype = toktype
+		last_col = ecol
+		last_lineno = elineno
+
+
+	# os.system('if [ -f '+dest+' ]; then echo '+dest+' found; fi')
+	# with open(fname,'wb') as wfd:
+	# 	with open('temp','rb') as fd:
+	# 		shutil.copyfileobj(fd, wfd)
 
 def comment_remover_cpp(text):
     def replacer(match):
@@ -71,7 +77,7 @@ if __name__ == "__main__":
 	outpPng = '../src/assets/results/outpImg.png'
 	outpHeatmap = '../src/assets/results/outpHeatmap.png'
 	# n such that top n results are displayed in the bar graph
-	barGraphParam = 5
+	barGraphParam = 10
 
 
 	zipFilePath = ''
@@ -91,19 +97,42 @@ if __name__ == "__main__":
 		tempF = os.path.join(directory, filenames[i])
 		fileExt = (os.path.splitext(tempF))[1]
 
+		### PREPROCESSING
 		if(fileExt == '.py'):
+			try:
+				with open(tempF,'r') as f:
+					python_code = f.read()
+					# root = ast.parse(python_code)
+					# names = sorted({node.id for node in ast.walk(root) if isinstance(node, ast.Name)})
+					line = re.sub('\w+(?=\()', 'fnCall', python_code)
+				with open(tempF,'w') as f:
+					f.write(line)
+			except:
+				print(tempF + " does not compile.")
 			comment_remover_py(tempF)
-		
-		if(fileExt == '.cpp'):
-			os.system('bash bashTest.sh ' + tempF)
-			os.system('cat temp > ' + tempF)
-			os.system('rm temp')
+			os.system('cat temp > '+tempF)
+							
+		elif(fileExt == '.cpp'):
+			try:
+				os.system('bash bashTest.sh ' + tempF)
+				os.system('cat temp > ' + tempF)
+				os.system('rm temp')
+			except Exception as e:
+				print(tempF + " does not compile.")
+				with open(tempF, 'r') as readF:
+					f = readF.read()
+					f = comment_remover_cpp(f);
+					tempF.write(f)
+
+			
 		
 		with open(tempF,'r') as readF:
 
 			f = readF.read()
-			f = f.replace(';', ' ')
+			f = f.lower()
+			f = f.replace(';', ' ').replace('\n', ' ')
 			f = re.sub(r"\s\s+", ' ', f);
+			f = re.sub('fncall', 'F', f);
 			# print(tempF)
 			# print(f)
 			for word in f.split():
@@ -113,6 +142,8 @@ if __name__ == "__main__":
 
 	baseArray = np.array(list(baseDict.values()))
 	
+	# print(baseArray.shape)
+
 	u, s, v = np.linalg.svd(baseArray, False)
 	lowRank = 1+int((baseArray.shape[0])/5)
 
@@ -121,6 +152,8 @@ if __name__ == "__main__":
 
 	if(lowRank > 300):
 		lowRank = 300
+
+	# print(baseArray.shape[0], lowRank)
 
 	uRed = u[:lowRank, :]
 	sRed = np.diag(s[:lowRank])
@@ -142,9 +175,11 @@ if __name__ == "__main__":
 				f.write(str(abs(round(finalRes[min(i,j),max(i,j)],3))) + ",")
 			f.write("\n")
 	
+	finalRes = np.power(finalRes, 6)
+
 	tempRes = np.array(finalRes, copy=True)
 	tempRes = np.maximum(tempRes, tempRes.transpose())
-
+	
 	for i in range(numFiles):
 		finalRes[i,i] = 0
 
@@ -156,23 +191,20 @@ if __name__ == "__main__":
 	for pos in range(numDisp):
 		dicTop5[finalRes[np.argmax(finalRes)]] = str(filenames[np.argmax(finalRes) % numFiles]) + " and\n" + str(filenames[np.argmax(finalRes) // numFiles])
 		finalRes[np.argmax(finalRes)] = 0
-	top5Names = [key for key in sorted(dicTop5)]
-	top5Coeffs = [dicTop5[key] for key in top5Names]
+	top5Coeffs = [key for key in sorted(dicTop5)]
+	top5Names = [dicTop5[key] for key in top5Coeffs]
 	
 	fig, ax = plt.subplots()
-	ax.barh(range(len(dicTop5)), top5Names)
-	plt.yticks(range(len(dicTop5)), top5Coeffs)
+	ax.barh(range(len(dicTop5)), top5Coeffs)
+	plt.yticks(range(len(dicTop5)), top5Names)
 
-	for index, value in enumerate(top5Names):
+	for index, value in enumerate(top5Coeffs):
 	    plt.text(value, index, str(round(value,2)))
 	right_side = ax.spines["right"]
 	right_side.set_visible(False)
 
 	plt.tight_layout()
 	plt.savefig(outpPng)
-	
-	# finalRes = np.reshape(finalRes, (numFiles,numFiles))
-	# print(finalRes)
 
 	fig, ax = plt.subplots()
 	# print(finalRes.shape)
@@ -199,9 +231,10 @@ if __name__ == "__main__":
 	ax.set_title("Heatmap of degree of plagiarism")
 	fig.tight_layout()
 	plt.savefig(outpHeatmap)
+	# print(top5Coeffs)
 	
 	try:
 		os.system('rm -r inputDir')
-		# os.system('rm temp')
+		os.system('if [ -f temp ]; then rm temp; fi')
 	except OSError as error:
 		print("Directory does not exist.")
