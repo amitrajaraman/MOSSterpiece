a5eaf603ecf9bf665cf8b586e7685302764d9b8b
Akash Cherukuri <190050009@iitb.ac.in>
2020-12-06 23:43:29 +0530

    update

--- src/app/waiting/waiting.component.ts
+++ src/app/waiting/waiting.component.ts
@@ -10,26 +10,20 @@ import { Router } from '@angular/router'
 })
 export class WaitingComponent implements OnInit {
   filename:string;
-  done:boolean;
   
-  constructor(private service:SharedService, private resultservice:ResultService, public messengerService: MessengerService, private router: Router, private fileService: FileService) {
-    this.filename="default";
-    this.done = false;
-    console.log(this.filename);
-  }
+  constructor(private service:SharedService, private resultservice:ResultService, public messengerService: MessengerService, private router: Router, private fileService: FileService) {  }
 
   async ngOnInit(){
-    // Waits for the processing to be completed
-    if(!this.done){await this.Process();}
+    this.Process();
   }
 
   async Process(){
     this.filename = this.fileService.getMessage();
-    const formdata = {"file": this.filename};
-    const t = await this.service.processFile(formdata).toPromise();
+    if(this.filename!=""){
+      const formdata = {"file": this.filename};
+      const t = await this.service.processFile(formdata).toPromise();
+    }
     //reroute after process is done
     this.router.navigate(['/upload/view']);
-    this.done=true;
   }
-
 }
