ce409533ff1d7577b9784cecabd12e1fd19cdb80
amitrajaraman <amit.rajaraman@gmail.com>
2020-12-07 23:06:03 +0530

    wrote some SICK documentation

--- backend_LSA/bashTest.sh
+++ backend_LSA/bashTest.sh
@@ -1,4 +1,19 @@
+# This code compiles the code using g++, but terminates after the pre-processing step.
+# This does two things: remove all macros (such as #define and #include) and replaces them with the
+# raw code in a file. Sending the output to temp creates a large file with this.
 g++ -E $1 > temp
+# Now, we don't want the massive amounts of code which are present because of the replacement of the
+# (#include) macros - not replacing them results in a file of over 100000 lines.
+# https://gcc.gnu.org/onlinedocs/cpp/Preprocessor-Output.html
+# Going through the official documentation shows that when we start a section caused by a #include,
+# it should be of the form
+# # linenum "library name" 1 3
+# and ends with
+# # linennum "file name" 2
+# Use sed to delete all the intermediate lines
 sed '/^# [0-9]* ".*" 1 3/,/# [0-9]* ".*" 2$/d' temp > temp1
+# There finally remain some excess lines that begin with a # that we do not require.
+# Use sed to delete these unnecessary lines
 sed '/^#/d' temp1 > temp
+# Delete the temporary created file
 rm temp1
--- backend_LSA/mainBack.py
+++ backend_LSA/mainBack.py
@@ -8,8 +8,10 @@ from zipfile import ZipFile
 import matplotlib
 import matplotlib.pyplot as plt
 import subprocess
-# import ast
 
+"""
+Code for creating heatmap taken from https://matplotlib.org/3.1.1/gallery/images_contours_and_fields/image_annotated_heatmap.html
+"""
 def heatmap(data, row_labels, col_labels, ax=None, cbar_kw={}, cbarlabel="", **kwargs):
     """
     Create a heatmap from a numpy array and two lists of labels.
@@ -69,6 +71,9 @@ def heatmap(data, row_labels, col_labels, ax=None, cbar_kw={}, cbarlabel="", **k
 
     return im, cbar
 
+"""
+Code for annotating heatmap taken from https://matplotlib.org/3.1.1/gallery/images_contours_and_fields/image_annotated_heatmap.html
+"""
 def annotate_heatmap(im, data=None, valfmt="{x:.2f}", textcolors=["black", "white"], threshold=None, **textkw):
     """
     A function to annotate a heatmap.
@@ -125,6 +130,10 @@ def annotate_heatmap(im, data=None, valfmt="{x:.2f}", textcolors=["black", "whit
 
     return texts
 
+"""
+Code for removing comments (from a py file) taken from  https://gist.github.com/BroHui/aca2b8e6e6bdf3cb4af4b246c9837fa3
+with minor changes done to bring it into the format we require
+"""
 def comment_remover_py(fname):
 	source = open(fname)
 	dest = 'temp'
@@ -159,12 +168,12 @@ def comment_remover_py(fname):
 		last_col = ecol
 		last_lineno = elineno
 
-
-	# subprocess.call('if [ -f '+dest+' ]; then echo '+dest+' found; fi')
-	# with open(fname,'wb') as wfd:
-	# 	with open('temp','rb') as fd:
-	# 		shutil.copyfileobj(fd, wfd)
-
+"""
+Code for removing comments (from a CPP file) taken from https://stackoverflow.com/questions/241327/remove-c-and-c-comments-using-python
+Note that we do not necessarily require this due to the code of bashTest.sh, but we keep it in the case that
+the code does not compile (so g++ won't work).
+Since comments in Java files are similar, we use the same function there as well.
+"""
 def comment_remover_cpp(text):
     def replacer(match):
         s = match.group(0)
@@ -178,6 +187,10 @@ def comment_remover_cpp(text):
     )
     return re.sub(pattern, replacer, text)
 
+"""
+This function merely returns the cosine similarity of two vectors (stored as numpy.ndarrays)
+We use scipy.spatial distance tosimplify the process
+"""
 def cosine_similarity(c1, c2):
 	if(c1.shape != c2.shape):
 		print("ERROR"),
@@ -185,14 +198,15 @@ def cosine_similarity(c1, c2):
 	return (1 - scipy.spatial.distance.cosine(c1, c2))
 
 if __name__ == "__main__":
+
+	# Output for the purpose of debugging to ensure that everything works as intended
 	print(sys.argv[1])
-	print("RUNNING MY CODE NOW!!!")
-	# subprocess.call('cd ../backend_LSA')
+	print("RUNNING AMIT'S CODE NOW!!!")
 	# Location of the (temporary) directory the zip file is unzipped to
 	directory = '../backend_LSA/inputDir'
+	# Location of the Bash code used for C++ pre-processing
 	bashLoc = '../backend_LSA/bashTest.sh'
-	# subprocess.call('if [[ -d '+directory+' ]]; then rm -r '+directory+'; fi')
-	# Location of the output CSV file
+	# Location of the output file(s) which store the raw data
 	outpFile = '../src/assets/results/outpFile.txt'
 	outpCSV = '../src/assets/results/outpFile.csv'
 	top = '../src/assets/results/top.txt'
@@ -203,6 +217,7 @@ if __name__ == "__main__":
 	barGraphParam = 10
 
 
+	# Unzip the argument given to the directory specified by the variable directory (inputDir)
 	zipFilePath = ''
 	try:
 		zipFilePath = sys.argv[1]
@@ -212,88 +227,137 @@ if __name__ == "__main__":
 		print("Usage: mainBack.py relative/path/to/filename.zip")
 		print("Quitting")
 		quit()
+
+	# Create a list of all the file names as stored in the directory and get the number of files
 	filenames = [name for name in os.listdir(directory)]
 	numFiles = len(filenames)
-	baseDict = {}	
+	# This variable stores the frequency of each word in each document
+	baseDict = {}
 
 	for i in range(numFiles):
+
+		# Create a path to the current file that is being processed
 		tempF = os.path.join(directory, filenames[i])
+		# Get the extension so we know what pre-processing to apply
 		fileExt = (os.path.splitext(tempF))[1]
 
 		### PREPROCESSING
+
 		if(fileExt == '.py'):
 			print("Processing "+tempF)
+			# Use the function defined above to remove all comments
 			comment_remover_py(tempF)
+			# Since the output of the function is stored in the file temp, we pipe it back to the normal .py file
+			# to make future processing easier and more uniform
 			subprocess.call('cat temp > '+tempF, shell=True)
 							
 		elif(fileExt == '.cpp'):
 			print("Processing "+tempF)
 			try:
+				# Process the CPP file (assuming it compiles) to remove all macros and comments with the output in the file temp
 				subprocess.call('bash ' + bashLoc + ' ' + tempF, shell = True)
+				# Since the output of the function is stored in the file temp, we pipe it back to the normal .cpp file
+				# to make future processing easier and more uniform
 				subprocess.call('cat temp > ' + tempF, shell = True)
+				# Remove the temporary file (not necessary but we do it anyway)
 				subprocess.call('rm temp', shell = True)
 			except Exception as e:
 				print(e)
 				print(tempF + " does not compile.")
 			with open(tempF, 'r') as readF:
 				f = readF.read()
+				# Although unnecessary if it compiles, we remove all comments once again for safety's sake
 				f = comment_remover_cpp(f);
+				# Write the output back to the cpp file to make future processing more uniform
 				with open(tempF, 'w') as writeF:
 					writeF.write(f)	
 		elif(fileExt == '.java'):
 			print("Processing "+tempF)
 			with open(tempF, 'r') as readF:
 				f = readF.read()
+				# Since Java comments are extremely similar to C++ comments, we can use the same function for both
 				f = comment_remover_cpp(f);
+				# Write the output back to the cpp file to make future processing more uniform
 				with open(tempF, 'w') as writeF:
 					writeF.write(f)	
 
 		with open(tempF,'r') as readF:
-
 			f = readF.read()
+			# Make everything lower case to remove case distinctions
 			f = f.lower()
+			# Remove all ;s and newlines
 			f = f.replace(';', ' ').replace('\n', ' ')
+			# Remove all whitespace, returning contiguous blocks with a single space
 			f = re.sub(r"\s\s+", ' ', f);
-			f = re.sub('fncall', 'F', f);
-			# print(tempF)
-			# print(f)
+			# f = re.sub('fncall', 'F', f);
+			
+			# Instantiate the dictionary
 			for word in f.split():
 				if(word not in baseDict.keys()):
 					baseDict[word] = [0] * numFiles
+				# Finally, baseDict[word][i] represents the frequency of word in file number i
 				baseDict[word][i] += 1
 
+	# Convert it into a numpy array to make future processing simpler
 	baseArray = np.array(list(baseDict.values()))
 	
-	# print(baseArray.shape)
-
+	"""
+	First, an array is created where the *i,j*th entry represents the occurrence of word *j* in the *i*th document.
+	LSA essentially works by detecting the latent similarities between two documents. For example, if the word "dog"
+	and "hound" are used similarly in different documents (say we have the sentence "The dog barked at the car" in one
+	document and "The hound barked at the car" in another document), then it inherently identifies that the two words
+	might mean the same thing. A low-rank approximation of the original array is found by performing Singular Value
+	Decomposition and then taking the appropriate rows/columns. When we do this, it clumps together dimensions (which
+	are words here) that mean similar things. So if we had the words {(dog),(hound),(bed)} in the original document, a
+	low-rank approximation of this could correspond to something like {(1.43*dog + 0.39*hound),(bed)}. That is, when words
+	are clumped together, an appropriate weight is given to each word such that it carries latent similarities. After
+	these words are grouped together, the cosine similarity between two vectors (in the low-rank approximation) is
+	returned as the degree of plagiarism between them.
+	"""
+	# This particular line performs the SVD part of the algorithm
 	u, s, v = np.linalg.svd(baseArray, False)
+	# While there is no clear-cut method to pick an optimal rank immediately, we tested quite a bit and this seems to
+	# give good results
 	lowRank = 1+int((baseArray.shape[0])/5)
 
 	if(lowRank < numFiles):
 		lowRank = numFiles+1
 
+	# For extremely large dimension, some research papers seem to suggest that this is a reasonably optimal rank to choose
+	# https://www.aclweb.org/anthology/U15-1009.pdf
 	if(lowRank > 300):
 		lowRank = 300
 
+	# Perform the actual rank-lowering by taking the first lowRank largest singular values
 	uRed = u[:lowRank, :]
 	sRed = np.diag(s[:lowRank])
 	arrRed = np.dot(np.dot(uRed, sRed), v)
 
+	# This array will later store the final degrees of plagiarism
 	finalRes = np.zeros((numFiles,numFiles));
 
+	# Not really needed but meh
 	arrRed = np.round(arrRed, 7)
-
 
 	with open(outpFile, 'w') as f:
 		for i in range(numFiles):
+			# Set diagonal element to 1 since the similarity between two identical files is 1 anyway
 			finalRes[i,i] = 1.0
 			for j in range(numFiles):
 				if(i < j):
+					# The degree of plagiarism is just the cosine similarity between the reduced vectors
 					finalRes[i,j] = str(cosine_similarity(arrRed[:, i], arrRed[:, j]))	
-	
+	"""
+	What we noticed is that for CPP or Java files, the general value returned seems to be somewhat high.
+	However, there is still a reasonably large divide (~0.1) between actually plagiarised files and files that
+	are not plagiarised but return a high value nevertheless.
+	This issue is mitigated by raising each element to the 6th power so smaller numbers rapidly dwindle
+	whereas the actual plagiarised documents' values remain reasonably high
+	"""
 	if(fileExt == '.cpp' or fileExt == '.java'):
 		finalRes = np.power(finalRes, 6)
 
+	# Write the output into the text file so the website can read it for the live display
 	with open(outpFile, 'w') as f:
 		for i in range(numFiles):
 			f.write(filenames[i]+",")
@@ -304,6 +368,7 @@ if __name__ == "__main__":
 				f.write(str(abs(round(finalRes[min(i,j),max(i,j)],3))) + ",")
 			f.write("\n")
 		
+	# Write the output into a CSV in case the user wants to download the results
 	with open(outpCSV, 'w') as f:
 		for i in range(numFiles):
 			f.write(filenames[i]+",")
@@ -315,27 +380,31 @@ if __name__ == "__main__":
 			f.write("\n")
 
 
+	# Note that finalRes is an upper triangular matrix.
+	# Doing this "mirrors" the array to make it symmetrical throughout
 	tempRes = np.array(finalRes, copy=True)
 	tempRes = np.maximum(tempRes, tempRes.transpose())
 	
+	# Set all the diagonal elements to 0 so that they aren't (incorrectly) returned as the pairs
+	# that are most similar
 	for i in range(numFiles):
 		finalRes[i,i] = 0
-
 	
+
+	# Reshape so that we can just use argmax to find the highest similarity
 	finalRes = np.reshape(finalRes, (numFiles*numFiles))
+	# In case barGraphParam(=10) is more than the number of pairs, then lower it
 	numDisp = int(min(barGraphParam, numFiles*(numFiles-1)/2))
 	maxInd = [0 for x in range(numDisp)]
 	dicTop5 = {}
 	for pos in range(numDisp):
+		# Find the current highest similarity and immediately set it to 0 so that the same value is not returned again
+		# in future iterations
 		dicTop5[finalRes[np.argmax(finalRes)]] = str(filenames[np.argmax(finalRes) % numFiles]) + " and " + str(filenames[np.argmax(finalRes) // numFiles])
 		finalRes[np.argmax(finalRes)] = 0
 	top5Coeffs = [key for key in sorted(dicTop5)]
 	top5Names = [dicTop5[key] for key in top5Coeffs]
 	
-	fig, ax = plt.subplots()
-	ax.barh(range(len(dicTop5)), top5Coeffs)
-	plt.yticks(range(len(dicTop5)), top5Names)
-
 	with open(top, 'w') as f:
 		for i in top5Coeffs:
 			f.write(str(i)+",")
@@ -344,25 +413,34 @@ if __name__ == "__main__":
 			f.write(str(i)+",")
 		f.write("\n")
 
+	# Plot the horizontal bar graph of the top barGraphParam(=10) most similar pairs
+	fig, ax = plt.subplots()
+	ax.barh(range(len(dicTop5)), top5Coeffs)
+	plt.yticks(range(len(dicTop5)), top5Names)
+
+
 	for index, value in enumerate(top5Coeffs):
-	    plt.text(value, index, str(round(value,2)))
+		plt.text(value, index, str(round(value,2)))
+	# Remove the right margin so that the text does not overflow
 	right_side = ax.spines["right"]
 	right_side.set_visible(False)
 
 	plt.tight_layout()
+	# Print the bar graph image into outpPng
 	plt.savefig(outpPng)
 
+	# Create the pretty heatmap
 	fig, ax = plt.subplots()
-	im, cbar = heatmap(tempRes, filenames, filenames, ax=ax,
-                   cmap="BuPu", cbarlabel="Degree of similarity")
+	im, cbar = heatmap(tempRes, filenames, filenames, ax=ax, cmap="BuPu", cbarlabel="Degree of similarity")
+	# If there are 15 or more files, then don't annotate the heatmap because that overcrowds the map
+	# and makes it look very bad
 	if(numFiles < 15):
 		texts = annotate_heatmap(im, valfmt="{x:.2f}")
 
 	fig.tight_layout()
 	plt.savefig(outpHeatmap)
-	# plt.show()
-	# print(top5Coeffs)
 	
+	# Remove all the temporary files created in the process of running the code
 	try:
 		subprocess.call('rm -r '+directory, shell = True)
 		subprocess.call('if [ -f temp ]; then rm temp; fi', shell = True)
